1e0544955a5ae3fba256d327445df150ab5db7df
Duovyds <k.duovyds@gmail.com>
2024-09-15 16:15:45 +0300

    Initial Commit

---
+++ objektinis_pragramavimas_1_uzduotis/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  objektinis_pragramavimas_1_uzduotis
+//
+//  Created by Dovydas Krištupas on 15/09/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
