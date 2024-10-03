a106d0571133cde75cc5475f25066b2d5be02daf
Duovyds <k.duovyds@gmail.com>
2024-10-03 15:28:54 +0300

    Sukurti nauji .cpp ir .h failai v0.2 užduočiai atlikti. Pradėta kurti funkcija failu_generavimas, kuri generuos failus pagal pasirinktus parametrus. Šiuo metu ši funkcija sukuria failo šabloną su atitinkamais stulpeliais.

---
+++ objektinis_pragramavimas_1_uzduotis/Failas.cpp
@@ -0,0 +1,8 @@
+//
+//  Failas.cpp
+//  objektinis_pragramavimas_1_uzduotis
+//
+//  Created by Dovydas Krištupas on 03/10/2024.
+//
+
+#include <stdio.h>
---
+++ objektinis_pragramavimas_1_uzduotis/Failas.h
@@ -0,0 +1,12 @@
+//
+//  Failas.h
+//  objektinis_pragramavimas_1_uzduotis
+//
+//  Created by Dovydas Krištupas on 03/10/2024.
+//
+
+#ifndef Failas_h
+#define Failas_h
+
+
+#endif /* Failas_h */
