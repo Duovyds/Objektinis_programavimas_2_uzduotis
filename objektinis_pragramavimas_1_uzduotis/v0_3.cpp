555f9260f88f9e63e21d92fe9b01b42588309a88
Duovyds <k.duovyds@gmail.com>
2024-10-18 22:43:28 +0300

    Sukurti cpp ir header failai v0.3 versijai atlikti. Sukurta funkcija, kuri atrenka vargšiukus ir juos įrašo į list konteinerį.

---
+++ objektinis_pragramavimas_1_uzduotis/v0_3.cpp
@@ -0,0 +1,8 @@
+//
+//  v0_3.cpp
+//  objektinis_pragramavimas_1_uzduotis
+//
+//  Created by Dovydas Krištupas on 18/10/2024.
+//
+
+#include "v0_3.hpp"
---
+++ objektinis_pragramavimas_1_uzduotis/v0_3.hpp
@@ -0,0 +1,13 @@
+//
+//  v0_3.hpp
+//  objektinis_pragramavimas_1_uzduotis
+//
+//  Created by Dovydas Krištupas on 18/10/2024.
+//
+
+#ifndef v0_3_hpp
+#define v0_3_hpp
+
+#include <stdio.h>
+
+#endif /* v0_3_hpp */
