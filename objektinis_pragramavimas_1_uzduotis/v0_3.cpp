bbdfaa554973df0cb8292b0f35cfd7a56f15ce60
Duovyds <k.duovyds@gmail.com>
2024-10-22 17:00:36 +0300

    Sukurta funkcija failų įrašymui naudojant list konteinerį.

--- objektinis_pragramavimas_1_uzduotis/v0_3.cpp
+++ objektinis_pragramavimas_1_uzduotis/v0_3.cpp
@@ -60,6 +60,32 @@ void irasymas(list<Studentas>& studentai, string failo_pav, int pasirinkimas){
 
 
 
+void irasymas_naudojant_list(list<Studentas>& studentai, string failo_pav, int pasirinkimas){
+    
+    string failo_pavadinimas = string("/Users/dovydaskr/Documents/C++/objektinis_pragramavimas_1_uzduotis/objektinis_pragramavimas_1_uzduotis/") + failo_pav + ".txt";
+    
+    ofstream rez;
+    rez.open(failo_pavadinimas);
+    
+    // Funkcija, kuri rusiuoja studentus pagal ju pavardes didejimo tvarka. (Tvarka galima pakeisti palyginti_pavardes funkcijos viduje).
+    if (pasirinkimas == 1) {
+        studentai.sort(palyginti_vardus);
+    } else if (pasirinkimas == 2){
+        studentai.sort(palyginti_pavardes);
+    }
+    
+    rez << left << setw(20) << "Pavardė" << setw(20) << "Vardas" << setw(20) << "Galutinis (vid.)" << endl;
+        rez << "-----------------------------------------------------------------" << endl;
+    for (const auto& student: studentai) {
+        rez << left << setw(20) << student.pavarde << setw(20) << student.vardas
+        << setw(20) << fixed << setprecision(2) << student.galutinis_vid << endl;
+    }
+    
+    rez.close();
+}
+
+
+
 void laiko_skaiciavimas_list_konteineris(string failo_pavadinimas, int rikiavimo_pasirinkimas){
     
     vector<Studentas> studentai2;
