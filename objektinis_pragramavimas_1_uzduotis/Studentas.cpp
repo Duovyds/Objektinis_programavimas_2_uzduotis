29508c3e71587f8ae99d945b093f9452c157580a
Duovyds <k.duovyds@gmail.com>
2024-11-18 15:47:55 +0200

    Sutvarkyta išvedimo funkcija.

--- objektinis_pragramavimas_1_uzduotis/Studentas.cpp
+++ objektinis_pragramavimas_1_uzduotis/Studentas.cpp
@@ -402,25 +402,9 @@ void isvedimas_list(list<Studentas> studentai){
     << "Objekto adresas" << endl;
         cout << "----------------------------------------------------------------------------------------------" << endl;
     for (const auto& student: studentai) {
-        cout << left << setw(20) << student.pavarde << setw(20) << student.vardas
-        << setw(20) << fixed << setprecision(2)<< student.galutinis_vid
-        << setw(20) << fixed << setprecision(2) << student.galutinis_med
+        cout << left << setw(20) << student.getPavarde() << setw(20) << student.getVardas()
+        << setw(20) << fixed << setprecision(2)<< student.getGalutinisVid()
+        << setw(20) << fixed << setprecision(2) << student.getGalutinisMed()
         << setw(20) << &student << endl;
     }
-}
-
-
-
-// Konstructorius
-Studentas::Studentas(string vardas, string pavarde, vector<int> namu_darbai, int egzaminas){
-    this->vardas = vardas;
-    this->pavarde = pavarde;
-    this->namu_darbai = namu_darbai;
-    this->egzaminas = egzaminas;
-}
-
-
-// Destruktorius
-Studentas::~Studentas() {
-    cout << "Istrintas " << vardas << " " << pavarde << " objektas." << endl;
 }
