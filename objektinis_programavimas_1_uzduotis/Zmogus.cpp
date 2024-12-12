e015b06ff4e8c081b8a9e83c0d582a61f3761ae4
Duovyds <k.duovyds@gmail.com>
2024-12-12 20:30:11 +0200

    Pridėti komentarai Zmogus.cpp ir Zmogus.h failams.

--- objektinis_programavimas_1_uzduotis/Zmogus.cpp
+++ objektinis_programavimas_1_uzduotis/Zmogus.cpp
@@ -1,10 +1,27 @@
+/**
+ * @file Zmogus.cpp
+ * @brief Implementation of the `Zmogus` class, representing a basic person entity.
+ */
 #include <stdio.h>
 #include "Zmogus.h"
 #include "Header.h"
 
 
-// Default konstruktorius
+/**
+ * @brief Default constructor for the `Zmogus` class.
+ *
+ * Initializes the `Zmogus` object with empty strings for the first name (`vardas`)
+ * and last name (`pavarde`).
+ */
 Zmogus::Zmogus() : vardas(""), pavarde("") {}
 
-// Konstruktorius
+/**
+ * @brief Parameterized constructor for the `Zmogus` class.
+ *
+ * Initializes the `Zmogus` object with specified first name (`vardas`)
+ * and last name (`pavarde`).
+ *
+ * @param vardas The first name of the person.
+ * @param pavarde The last name of the person.
+ */
 Zmogus::Zmogus(string vardas, string pavarde): vardas(vardas), pavarde(pavarde) {}
--- objektinis_programavimas_1_uzduotis/Zmogus.h
+++ objektinis_programavimas_1_uzduotis/Zmogus.h
@@ -1,25 +1,62 @@
+/**
+ * @file Zmogus.h
+ * @brief Declaration of the `Zmogus` class, a base class for representing a person entity.
+ */
 #ifndef Zmogus_h
 #define Zmogus_h
 #include "Header.h"
 
 
+/**
+ * @class Zmogus
+ * @brief Represents a basic person entity with a name and surname.
+ *
+ * The `Zmogus` class provides protected attributes for a person's first name (`vardas`)
+ * and last name (`pavarde`). It is designed as a base class with pure virtual methods
+ * for accessing these attributes, making it abstract.
+ */
 class Zmogus {
 protected:
+    /**
+     * @brief The first name of the person.
+     */
     string vardas;
+    
+    /**
+     * @brief The last name of the person.
+     */
     string pavarde;
 
 public:
+    /**
+     * @brief Default constructor.
+     */
     Zmogus();
+    
+    /**
+     * @brief Parameterized constructor.
+     * @param vardas The first name of the person.
+     * @param pavarde The last name of the person.
+     */
     Zmogus(string vardas, string pavarde);
+    
+    /**
+     * @brief Virtual destructor.
+     */
     virtual ~Zmogus() = default;
     
     
-    // Geteriai
+    /**
+     * @brief Pure virtual getter for the first name.
+     * @return A constant reference to the `vardas` string.
+     */
     virtual const std::string& getVardas() const = 0;
+    
+    /**
+     * @brief Pure virtual getter for the last name.
+     * @return A constant reference to the `pavarde` string.
+     */
     virtual const std::string& getPavarde() const = 0;
-    
-    
-    
     
 };
 
